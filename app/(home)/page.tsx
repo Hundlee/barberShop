222f2fc03c7247100238754479a0475da8504132
Hundlee <mesquitag884@gmail.com>
2024-01-31 16:28:32 -0300

    feat: add inital search components

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,11 +1,13 @@
 import { ptBR } from "date-fns/locale";
 import Header from "../_components/header";
 import { format } from "date-fns";
+import Search from "./_components/search";
 
 export default function Home() {
     return (
         <div>
             <Header />
+
             <div className="px-5 pt-5">
                 <h2 className="text-xl font-bold">Olá, Gabriel!</h2>
                 <p className="capitalize text-sm">
@@ -13,6 +15,10 @@ export default function Home() {
                         locale: ptBR,
                     })}
                 </p>
+            </div>
+
+            <div className="px-5 mt-6">
+                <Search />
             </div>
         </div>
     );
