2e599f3e9f82db7e06d304c9dadd4ea3c20c6f4d
Hundlee <mesquitag884@gmail.com>
2024-02-02 09:18:16 -0300

    update: imageUrl to service

--- prisma/migrations/20240202114734_add_initial_tables/migration.sql
+++ prisma/migrations/20240202114734_add_initial_tables/migration.sql
@@ -23,6 +23,7 @@ CREATE TABLE "Service" (
     "price" DECIMAL(10,2) NOT NULL,
     "barbershopId" TEXT NOT NULL,
     "description" TEXT NOT NULL,
+    "imageUrl" TEXT NOT NULL,
 
     CONSTRAINT "Service_pkey" PRIMARY KEY ("id")
 );
