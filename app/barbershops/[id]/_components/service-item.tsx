fac7acfadff26392b4e5d82e0022a9c0eb04ab2f
Hundlee <mesquitag884@gmail.com>
2024-02-03 11:49:17 -0300

    feat: ensure unauthenticated user is redirected to login on booking click

--- app/barbershops/[id]/_components/service-item.tsx
+++ app/barbershops/[id]/_components/service-item.tsx
@@ -1,13 +1,25 @@
+"use client";
+
 import { Button } from "@/app/_components/ui/button";
 import { Card, CardContent } from "@/app/_components/ui/card";
 import { Service } from "@prisma/client";
+import { signIn } from "next-auth/react";
 import Image from "next/image";
 
 interface ServiceItemProps {
     service: Service;
+    isAuthenticated?: boolean;
 }
 
-const ServiceItem = ({ service }: ServiceItemProps) => {
+const ServiceItem = ({ service, isAuthenticated }: ServiceItemProps) => {
+    const handleBookingClick = () => {
+        if (!isAuthenticated) {
+            return signIn("google");
+        }
+
+        // TODO: Abrir modal de agendamento
+    };
+
     return (
         <Card>
             <CardContent className="p-3">
@@ -35,7 +47,12 @@ const ServiceItem = ({ service }: ServiceItemProps) => {
                                 }).format(Number(service.price))}
                             </p>
 
-                            <Button variant="secondary">Reservar</Button>
+                            <Button
+                                variant="secondary"
+                                onClick={handleBookingClick}
+                            >
+                                Reservar
+                            </Button>
                         </div>
                     </div>
                 </div>
--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,6 +1,8 @@
 import { db } from "@/app/_lib/prisma";
 import BarbershopInfo from "./_components/barbershop-info";
 import ServiceItem from "./_components/service-item";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/app/api/auth/[...nextauth]/route";
 
 interface BarbershopDetailsPageProps {
     params: {
@@ -11,6 +13,8 @@ interface BarbershopDetailsPageProps {
 const BarbershopDetailsPage = async ({
     params,
 }: BarbershopDetailsPageProps) => {
+    const session = await getServerSession(authOptions);
+
     if (!params.id) {
         // TODO: redirecionar para home page
         return null;
@@ -37,7 +41,11 @@ const BarbershopDetailsPage = async ({
 
             <div className="px-5 flex flex-col gap-4 py-6">
                 {barbershop.services.map((service: any) => (
-                    <ServiceItem key={service.id} service={service} />
+                    <ServiceItem
+                        key={service.id}
+                        service={service}
+                        isAuthenticated={!!session?.user}
+                    />
                 ))}
             </div>
         </div>
