91a7f1adf9aa449bc58fbc77a7f91334751a2c02
Hundlee <mesquitag884@gmail.com>
2024-02-05 21:48:58 -0300

    feat: add user name to home page

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -34,7 +34,11 @@ export default async function Home() {
             <Header />
 
             <div className="px-5 pt-5">
-                <h2 className="text-xl font-bold">Olá, Gabriel!</h2>
+                <h2 className="text-xl font-bold">
+                    {session?.user
+                        ? `Olá, ${session.user.name?.split(" ")[0]}!`
+                        : `Olá, Faça seu login!`}
+                </h2>
                 <p className="capitalize text-sm">
                     {format(new Date(), "EEEE',' d 'de' MMMM", {
                         locale: ptBR,
